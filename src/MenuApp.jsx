012a22b9550f4ac29de910eb6f737cf49950ac8c
lantshoe <lantshoe@gmail.com>
2026-06-05 15:47:18 +0200

    fix: turn off dev model

--- src/MenuApp.jsx
+++ src/MenuApp.jsx
@@ -8,7 +8,7 @@ import AuthPage from "./AuthPage";
 
 const SUPABASE_URL  = "https://udawpaivdegqhlyvnffs.supabase.co";
 const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkYXdwYWl2ZGVncWhseXZuZmZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MDUxNTAsImV4cCI6MjA5NDA4MTE1MH0.2aPiAEdFq1S4NBQ-BUDjhGx4WpLzvvUMk_1e0njROWg";
-const DEV_MODE = true;
+const DEV_MODE = false;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
 
